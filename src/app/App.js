10a314ea9057e1069903413929b95a8a13d774e7
Romartiny <romortiny@gmail.com>
2024-01-30 23:54:25 +0300

    add yandex map, hero and features section + logic for buy products online

--- src/app/App.js
+++ src/app/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import './assets/App.css';
 import Header from "../components/Header/Header.jsx";
 import Hero from "../components/Hero/Hero.jsx";
-import Pros from "../components/Pros/Pros";
-import Contacts from "../components/Contacts/Contacts";
-import Intake from "../components/Intake/Intake";
+import Pros from "../components/Pros/Pros.jsx";
+import Contacts from "../components/Contacts/Contacts.jsx";
+import Intake from "../components/Intake/Intake.jsx";
 
 function App() {
   return (
